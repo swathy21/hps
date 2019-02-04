ed0ba3162c5f30c702478bf05a8740098ae7f402
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-02-04 14:02:07 +0530

    Create positivenum.c

---
+++ positivenum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+if(n=0)
+{
+print("number is zero");
+else if(n<0)
+{
+print("number is negative");
+else
+print("number is positive");
+}
+}
+getch();
+}
+
+
+
+
+
