8fc9b8779219b488ef0eb5b4f473f33d9d28e2e0
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-02-04 14:33:03 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0);
+{
+printf("number is even",n);
+}
+else
+printf("number is negative",n);
+getch();
+}
